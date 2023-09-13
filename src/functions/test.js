7b58126f491c8178f33d89b4560a536718d14b43
KampfZomby <kampfzomby@gmail.com>
2023-09-13 21:10:48 +0200

    modified:   src/functions/test.js

--- src/functions/test.js
+++ src/functions/test.js
@@ -1,5 +1,16 @@
-function test () {
-    console.log("Hello!");
-}
+async function test() {
+        const response = await fetch("https://flusteredtendercable.kampfzomby.repl.co/menu");
+        const menu = await response.json();
+        const hauptgerichte = menu.menu.Hauptgerichte;
+        const nachspeisen = menu.menu.Nachspeisen;
+
+        console.log(menu)
+
+        hauptgerichte.forEach(item => {
+            console.log(item.bezeichnung)
+        });
+        console.log(hauptgerichte)
+        console.log(nachspeisen)
+    }
 
 export default test;
